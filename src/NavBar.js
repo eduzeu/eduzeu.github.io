667475886c1ca3d17059dcc0f946a377909281e7
eduzeu <zeuedu1@gmail.com>
2024-10-18 20:16:16 -0400

    modified:   src/NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -134,7 +134,7 @@ const Navbar = () => {
         <StyledLink exact to="/" activeClassName="active">Home</StyledLink>
         <StyledLink to="/projects" activeClassName="active">Projects</StyledLink>
         <StyledLink to="/experience" activeClassName="active">Experience</StyledLink>
-        <Download href={require("../src/resume.pdf")} download="Eduardo_Hernandez_Resume.pdf" activeClassName="active">Resume</Download>
+        <Download href={require("../src/resume.pdf")} download="Resume.Eduardo.Hernandez.pdf" activeClassName="active">Resume</Download>
       </NavLinks>
     </NavigationBar>
   );
